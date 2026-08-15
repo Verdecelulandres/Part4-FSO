a524d95d1dd2e7f565d4da838a7a747b7e84546e
Andres Laverde <andres.laverde@protonmail.com>
2026-08-14 21:28:41 -0700

    update: finish 4.4; create total likes test suite

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return blogs.length > 0 ? 1 : 1;
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+  return blogs.reduce((sum, current) =>
+    sum + (current.likes ? current.likes : 0), 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
