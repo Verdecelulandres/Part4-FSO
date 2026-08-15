2b50bcf62d17f3f7647d04c68f0ba42ab7c49277
Andres Laverde <andres.laverde@protonmail.com>
2026-08-14 21:54:27 -0700

    update: finish 4.5; set-up favorite blog test suite

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,23 @@ const totalLikes = (blogs) => {
     sum + (current.likes ? current.likes : 0), 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  let mostLiked = {};
+  let maxLikes = 0;
+  blogs.forEach(b => {
+    if(b.likes > maxLikes) {
+      maxLikes = b.likes;
+      mostLiked = b;
+    }
+  });
+  return mostLiked;
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
