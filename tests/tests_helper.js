89bb57398dffefbe79585faff5c93cf087f07239
Andres Laverde <andres.laverde@protonmail.com>
2026-08-15 19:31:58 -0700

    refactor testlist

--- tests/tests_helper.js
+++ tests/tests_helper.js
@@ -51,6 +51,14 @@ const blogs = [
 
 const equalLikeBlogs = [
   {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 2,
+    __v: 0
+  },
+  {
     _id: '5a422ba71b54a676234d17fb',
     title: 'TDD harms architecture',
     author: 'Robert C. Martin',
