b10ad21abb452e2609bfcbd45f369a11169dea4e
Andres Laverde <andres.laverde@protonmail.com>
2026-08-15 19:45:02 -0700

    create: initial blog api tests

---
+++ tests/blogs_api.test.js
@@ -0,0 +1,30 @@
+const { test, after, beforeEach } = require('node:test');
+const assert = require('node:assert');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const helper = require('./tests_helper');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.blogs);
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('correct amount of blogs is returned', async () => {
+  const result = await api.get('/api/blogs');
+  assert.strictEqual(result.body.length, helper.blogs.length);
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
